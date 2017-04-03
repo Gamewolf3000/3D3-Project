8e7fa2adc07644e329af98288c0757dcd1d9afc9
ajx <ajx@BTHW0062>
2017-04-03 16:55:37 +0200

    probably not

--- Project3D3/MeshTestPS.hlsl
+++ Project3D3/MeshTestPS.hlsl
@@ -26,8 +26,9 @@ float4 main(vertexData data) : SV_Target
 	float2 xyCoords = data.pos.xy;
 	xyCoords += 1.0f;
 	xyCoords *= 0.5f;
+
 	//return float4(shadowMap.Sample(samplerState, xyCoords).xyz, 1.0f);
 
 	//return float4(1.0f, 1.0f, 1.0f, 1.0f);
-	return float4(shadowMap.Sample(samplerState, data.uv).xyz, 1.0f);
+	return float4(shadowMap.Sample(samplerState, data.uv).xyz, 1.0f) * shadowMap.Sample(samplerState, xyCoords).x;
 }
