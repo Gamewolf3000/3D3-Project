0bc72eb7a1f240e7eebe36235a17302bb76c839c
BTH <BTH@H470-0007>
2017-04-26 13:23:14 +0200

    Fixing Stuff?

--- Project3D3/RasterizingStagePS.hlsl
+++ Project3D3/RasterizingStagePS.hlsl
@@ -24,15 +24,15 @@ PSOutput main(in vertexData data) : SV_Target
 	outPut.normal = data.normal;
 	outPut.worldPos = data.wPos;
 
-	/*bool test = outPut.worldPos != 0;
+	bool test = outPut.worldPos != 0;
 
 	for (int i = 0; i < 2000; i++)
 	{
-		outPut.worldPos = float4(sampleTexture.Sample(samplerState, (data.uv+float2((i%75)*0.000002, (i % 3)*0.000002))).xyz, 1.0f) / outPut.worldPos;
+		outPut.worldPos = float4(sampleTexture.Sample(samplerState, (data.uv+float2((i%75)*0.000002, (i % 3)*0.000002))).xyz, 1.0f);
 		test = i;
 	}
 	if (test)
-		outPut.worldPos = data.wPos;*/
+		outPut.worldPos = data.wPos;
 
 	return outPut;
 }
