64beaf51bf1ca24df1c4a0c436a05f49f5803b4d
Andreas Jonasson <Andreas Jonasson@LAPTOP-6Q5NU2MN>
2017-04-26 11:11:13 +0200

    "Bad case" added

--- Project3D3/RasterizingStagePS.hlsl
+++ Project3D3/RasterizingStagePS.hlsl
@@ -1,9 +1,9 @@
 /*Finish the Rasterization*/
 struct PSOutput
 {
-    float4 normal : SV_TARGET0;
-    float4 colour : SV_TARGET1;
-    float4 worldPos : SV_TARGET2;
+	float4 normal : SV_TARGET0;
+	float4 colour : SV_TARGET1;
+	float4 worldPos : SV_TARGET2;
 };
 
 Texture2D sampleTexture : register(t0);
@@ -11,17 +11,26 @@ sampler samplerState : register(s0);
 
 struct vertexData
 {
-    float4 pos : SV_POSITION;
-    float4 wPos : WORLDPOS;
-    float4 normal : NORMAL;
-    float2 uv : TEXCOORDS;
+	float4 pos : SV_POSITION;
+	float4 wPos : WORLDPOS;
+	float4 normal : NORMAL;
+	float2 uv : TEXCOORDS;
 };
 
 PSOutput main(in vertexData data) : SV_Target
 {
-    PSOutput outPut;
-    outPut.colour = float4(sampleTexture.Sample(samplerState, data.uv).xyz, 1.0f);
-    outPut.normal = data.normal;
-    outPut.worldPos = data.wPos;
-    return outPut;
+	PSOutput outPut;
+	outPut.colour = float4(sampleTexture.Sample(samplerState, data.uv).xyz, 1.0f);
+	outPut.normal = data.normal;
+	outPut.worldPos = data.wPos;
+	bool test = outPut.worldPos != 0;
+
+	for (int i = 0; i < 2000; i++)
+	{
+		outPut.worldPos = float4(sampleTexture.Sample(samplerState, (data.uv + float2((i % 75)*0.000002, (i % 3)*0.000002))).xyz, 1.0f) / outPut.worldPos;
+		test = i;
+	}
+	if (test)
+		outPut.worldPos = data.wPos;
+	return outPut;
 }
